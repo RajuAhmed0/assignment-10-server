7f52ba69d5a7e2916e057cfeebea8fd6a3043654
RajuAhmed0 <itzraju98@gmail.com>
2025-03-07 00:05:29 +0600

    ve

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        await client.connect();
+        // await client.connect();
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
         // Database & Collection
